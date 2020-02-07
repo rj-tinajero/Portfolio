7e4bce497a8fee800f8fddadd9ac9357d6ff57d5
Richard Tinajero <rj.tinajero92@gmail.com>
2020-02-06 16:26:56 -0800

    first steps in initalizing project

---
+++ src/components/Navbar.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import NavItem from './NavItem';
+import { BrowserRouter } from 'react-router-dom';
+
+class Navbar extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         'NavItemActive': ''
+      }
+   }
+
+   activeItem = x => {
+      if(this.state.NavItemActive.length > 0) {
+         document.getElementById(this.state.NavItemActive).classList.remove('active');
+      }
+      this.setState({'NavItemActive': x}, () => {
+         document.getElementById(this.state.NavItemActive).classList.add('active');
+      });
+   }
+
+   render() {
+      return(
+         
+         <nav>
+            <ul>
+               <NavItem activec={this.activeItem} item="Home" toLink="/" />
+               <NavItem activec={this.activeItem} item="About" toLink="/about" />
+               <NavItem activec={this.activeItem} item="Education" toLink="/education" />
+               <NavItem activec={this.activeItem} item="Skills" toLink="/skills" />
+               <NavItem activec={this.activeItem} item="Contact" toLink="/contact" />
+            </ul>
+         </nav>
+        
+      );
+   }
+}
+
+export default Navbar;
---
+++ src/contents/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import profilepic from '../img/richard1.jpeg';
+
+class XYZ extends Component {
+   render() {
+      return(
+         <img src={profilepic} className="profilepic"></img>
+      )
+   }
+}
+
+export default Home; 
